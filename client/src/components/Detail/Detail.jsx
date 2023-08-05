a460f1191a7b72f0682f8c20d49af52674a12a85
MatiasDAppella <appellamatias@gmail.com>
2023-08-05 15:15:53 -0300

    detail css done

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,4 +1,9 @@
+// Styles & images
 import style from './Detail.module.less';
+import pokeball from '../../assets/img/pokeball.png';
+
+// Components
+import Tag from '../Home/Pokemons/Card/Tag/Tag';
 
 const pokemon = {
   "name": "farigiraf",
@@ -19,9 +24,29 @@ const pokemon = {
 const Detail = () => {
   return <main className={style.detail}>
     <div className={style.container}>
-      <div className={style.imageBox}></div>
-      <div className={style.infoBox}></div>
-      <div className={style.catchBox}></div>
+      <div className={style.imageBox}>
+        <span>{(pokemon.apiid) ? pokemon.apiid : '???'}</span>
+        <img src={pokemon.image} alt="pokémon image"/>
+      </div>
+
+      <div className={style.infoBox}>
+        <h1>{pokemon.name}</h1>
+        <div className={style.bodyInfo}>
+          <span>Height: {pokemon.height}</span>
+          <span>Weight: {pokemon.weight}</span>
+        </div>
+        <div className={style.stats}>
+          <span>Health: {pokemon.health}</span>
+          <span>Stroke: {pokemon.stroke}</span>
+          <span>Defense: {pokemon.defense}</span>
+          <span>Speed: {pokemon.speed}</span>
+        </div>
+        <div className={style.tags}>{
+          pokemon.types.map(type => <Tag key={type} type={type}/>)
+        }</div>
+      </div>
+        
+      <button className={style.catchButton}>Catch in pokeball</button>
     </div>
   </main>
 };
