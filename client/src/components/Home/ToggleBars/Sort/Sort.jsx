c28ae716b3bf994d78a4b31f01f626b4d597ce4f
MatiasDAppella <appellamatias@gmail.com>
2023-08-08 11:07:38 -0300

    constants deleted

--- client/src/components/Home/ToggleBars/Sort/Sort.jsx
+++ client/src/components/Home/ToggleBars/Sort/Sort.jsx
@@ -8,7 +8,6 @@ import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 
 // Actions & constants
-import { STROKE, DEFENSE, HEALTH, SPEED } from '../../../../constants';
 import {
   sortAscByStroke,
   sortDesByStroke,
@@ -33,19 +32,19 @@ const Sort = () => {
     const sort = event.target.id
 
     switch (sort){
-      case STROKE:
+      case "STROKE":
         if (toogleMethod.stroke) dispath(sortAscByStroke())
         else dispath(sortDesByStroke())
         return setToggleMethod({...toogleMethod, stroke: !toogleMethod.stroke})
-      case DEFENSE:
+      case "DEFENSE":
         if (toogleMethod.defense) dispath(sortAscByDefense())
         else dispath(sortDesByDefense())
         return setToggleMethod({...toogleMethod, defense: !toogleMethod.defense})
-      case HEALTH:
+      case "HEALTH":
         if (toogleMethod.health) dispath(sortAscByHealth())
         else dispath(sortDesByHealth())
         return setToggleMethod({...toogleMethod, health: !toogleMethod.health})
-      case SPEED:
+      case "SPEED":
         if (toogleMethod.speed) dispath(sortAscBySpeed())
         else dispath(sortDesBySpeed())
         return setToggleMethod({...toogleMethod, speed: !toogleMethod.speed})
@@ -53,10 +52,10 @@ const Sort = () => {
   };
   
   return <div className={style.sortBar}>
-    <button id={STROKE} onClick={clickHandler}><FontAwesomeIcon className={style.icon} icon={faHandFist}/>Stroke</button>
-    <button id={DEFENSE} onClick={clickHandler}><FontAwesomeIcon className={style.icon} icon={faShieldHalved}/>Defense</button>
-    <button id={HEALTH} onClick={clickHandler}><FontAwesomeIcon className={style.icon} icon={faHeart}/>Health</button>
-    <button id={SPEED} onClick={clickHandler}><FontAwesomeIcon className={style.icon} icon={faDragon}/>Speed</button>
+    <button id={"STROKE"} onClick={clickHandler}><FontAwesomeIcon className={style.icon} icon={faHandFist}/>Stroke</button>
+    <button id={"DEFENSE"} onClick={clickHandler}><FontAwesomeIcon className={style.icon} icon={faShieldHalved}/>Defense</button>
+    <button id={"HEALTH"} onClick={clickHandler}><FontAwesomeIcon className={style.icon} icon={faHeart}/>Health</button>
+    <button id={"SPEED"} onClick={clickHandler}><FontAwesomeIcon className={style.icon} icon={faDragon}/>Speed</button>
   </div>
 };
 
