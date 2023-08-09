d386a4f1e3718fa5dd14dde1834f268a921ddd28
MatiasDAppella <appellamatias@gmail.com>
2023-08-09 14:22:37 -0300

    footer added

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,20 +1,32 @@
-// Styles
+// Styles & icons
 import style from './Home.module.less';
+import githubIcon from '../../assets/icons/github.png'
+import instagramIcon from '../../assets/icons/instagram.png'
 
 // Hooks
 import { useState } from 'react';
 
 // Components
-import ToggleBars from './ToggleBars/ToggleBars';
 import Pokemons from './Pokemons/Pokemons';
 import Navigate from './Navigate/Navigate';
 
 const Home = () => {
 
-  return <main className={style.main}>
+  return <div className={style.homePage}>
     <Navigate/>
-    <Pokemons/>
-  </main>
+    <main>
+      <Pokemons/>
+    </main>
+    <footer>
+      <div className={style.footerContainer}>
+        <h3>Made by Matias Appella</h3>
+        <div className={style.socialLinks}>
+          <a href="https://github.com/MatiasDAppella" target='_blank'><img className={style.githubIcon} src={githubIcon} alt="" /></a>
+          <a href="https://www.instagram.com/matiiapp/" target='_blank'><img className={style.instagramIcon} src={instagramIcon} alt="" /></a>
+        </div>
+      </div>
+    </footer>
+  </div>
 };
 
 export default Home;
